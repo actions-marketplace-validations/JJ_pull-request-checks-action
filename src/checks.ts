ce7175d33fe51bd1c5c4acc0cd067f4309fc4b66
JJ Merelo <jjmerelo@gmail.com>
2020-11-01 20:39:45 +0100

    Cleans up a bit

--- src/checks.ts
+++ src/checks.ts
@@ -1,9 +1,9 @@
-const checklist = /\s*\-\s+\[\s*([xX]?)\s*\]\s+(.+?)\.\s+/mg;
+const checklist = /\s*-\s+\[\s*([xX]?)\s*\]\s+(.+?)\.\s+/gm
 
 
-export async function checks(body: string ): Promise<{ [id:string] : boolean}> {
+export async function checks(body: string): Promise<{[id: string]: boolean}> {
     return new Promise(() => {
-        let checked : { [id:string] : boolean} = {}
+        const checked: { [id: string] : boolean} = {}
         const match = checklist.exec(body)
         while (match != null) {
             checked[match[2]] = match[1]?true:false
