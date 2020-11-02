8768786338c3243d39335722539faf89d15e13fb
JJ Merelo <jjmerelo@gmail.com>
2020-11-02 08:53:40 +0100

    Links action

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import * as core from '@actions/core'
+const {GitHub, context} = require('@actions/github')
 import {checks} from './checks'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-//    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+      const body: string = context.payload.pull_request.body
+      core.debug(`Got ${body}`)
+      const checked = await checks( body )
+      core.exportVariable('checked', checked )
   } catch (error) {
-    core.setFailed(error.message)
+      core.setFailed(error.message)
   }
 }
 
