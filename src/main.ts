9d600f6810ffa64b19ecf9dd2c6abf385348a2c9
JJ Merelo <jjmerelo@gmail.com>
2020-11-02 09:53:33 +0100

    Debugging

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import * as core from '@actions/core'
-const {GitHub, context} = require('@actions/github')
+import * as github from '@actions/github'
 import {checks} from './checks'
 
 async function run(): Promise<void> {
-  try {
-      const body: string = context.payload.pull_request.body
-      core.debug(`Got ${body}`)
-      const checked = await checks( body )
-      core.exportVariable('checked', checked )
-      core.setOutput('checked',checked)
+    const context = github.context
+    try {
+        if ( !context.payload.pull_request ) {
+            core.setFailed("Only available for pull requests")
+        }
+        console.log( context.payload.pull_request )
+//        let body: string = context.payload.pull_request.body!
+//        core.debug(`Got ${body}`)
+//        const checked = await checks( body )
+//        core.exportVariable('checked', checked )
+//        core.setOutput('checked',checked)
   } catch (error) {
       core.setFailed(error.message)
   }
