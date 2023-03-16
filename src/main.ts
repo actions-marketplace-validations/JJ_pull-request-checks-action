595d95d9c09e998c2cab1ff59b50e2af19fbcdfd
JJ Merelo <jjmerelo@gmail.com>
2023-03-16 09:12:58 +0100

    :rotating_light: which needed a bit of refactoring

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,26 @@ import {checks} from './checks'
 async function run(): Promise<void> {
   const context = github.context
   try {
-    if (!context.payload.pull_request) {
+    if (
+      context.payload.pull_request !== null &&
+      context.payload.pull_request !== undefined
+    ) {
+      if (
+        'body' in context.payload.pull_request &&
+        context.payload.pull_request.body !== null &&
+        context.payload.pull_request.body !== undefined
+      ) {
+        const body: string = context.payload.pull_request.body
+        core.debug(`Got ${body}`)
+        const checked: {[id: string]: boolean} = await checks(body)
+        core.exportVariable('checked', checked)
+        core.setOutput('checked', checked)
+      } else {
+        core.setFailed('No body to check or anything else')
+      }
+    } else {
       core.setFailed('Only available for pull requests')
     }
-    const body: string | undefined = context.payload.pull_request?.body
-    core.debug(`Got ${body}`)
-    const checked: {[id: string]: boolean} = await checks(body)
-    core.exportVariable('checked', checked)
-    core.setOutput('checked', checked)
   } catch (error: Error | unknown) {
     if (error instanceof Error) {
       core.setFailed(error?.message)
