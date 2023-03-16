9aa6fe8991bad4664919f7f134f3a3676a471226
JJ Merelo <jjmerelo@gmail.com>
2023-03-16 17:13:59 +0100

    :white_check_mark: we need to eliminate the colon
    + debugh stuff

--- src/checks.ts
+++ src/checks.ts
@@ -5,18 +5,15 @@ export function checks(body: NonNullable<string>): {[id: string]: boolean} {
   let match = checklist.exec(body)
   let index = 0
   while (match != null) {
-    console.log(match[2])
     let key
     if (match[2] !== undefined) {
-      key = match[2]
+      key = match[2].replace(':', '')
     } else {
       key = `check${index}`
     }
-    console.log(key)
     checked[key] = match[1] ? true : false
     index++
     match = checklist.exec(body)
   }
-  console.log(checked)
   return checked
 }
