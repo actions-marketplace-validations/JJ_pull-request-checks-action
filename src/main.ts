c0bbc487aaf9f6b4d721ef02cf172bb809d2de58
JJ Merelo <jjmerelo@gmail.com>
2023-03-16 08:03:03 +0100

    Fix for release

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,18 @@ import * as github from '@actions/github'
 import {checks} from './checks'
 
 async function run(): Promise<void> {
-    const context = github.context
-    try {
-        if ( !context.payload.pull_request ) {
-            core.setFailed("Only available for pull requests")
-        }
-        console.log( context.payload.pull_request 
-        let body: string = context.payload.pull_request.body!
-        core.debug(`Got ${body}`)
-        const checked = await checks( body )
-        core.exportVariable('checked', checked )
-        core.setOutput('checked',checked)
-  } catch (error) {
-      core.setFailed(error.message)
+  const context = github.context
+  try {
+    if (!context.payload.pull_request) {
+      core.setFailed('Only available for pull requests')
+    }
+    const body: string = context.payload.pull_request.body!
+    core.debug(`Got ${body}`)
+    const checked = await checks(body)
+    core.exportVariable('checked', checked)
+    core.setOutput('checked', checked)
+  } catch (error: any) {
+    core.setFailed(error.message)
   }
 }
 
