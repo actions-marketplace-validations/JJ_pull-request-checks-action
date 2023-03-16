5e81ab0dec87dda6017f916851ea6e95f38e7f45
JJ Merelo <jjmerelo@gmail.com>
2023-03-16 15:22:01 +0100

    :white_check_mark:

--- src/checks.ts
+++ src/checks.ts
@@ -1,19 +1,22 @@
-const checklist = /\s*-\s+\[\s*([xX]?)\s*\]\s+([A-Z]*):?\s*/gm
+const checklist = /\s*-\s+\[\s*([xX]?)\s*\]\s+([A-Z]+:)?\s*/gm
 
 export function checks(body: NonNullable<string>): {[id: string]: boolean} {
   const checked: {[id: string]: boolean} = {}
   let match = checklist.exec(body)
   let index = 0
   while (match != null) {
+    console.log(match[2])
     let key
-    if (match[2] !== '') {
+    if (match[2] !== undefined) {
       key = match[2]
     } else {
       key = `check${index}`
     }
+    console.log(key)
     checked[key] = match[1] ? true : false
     index++
     match = checklist.exec(body)
   }
+  console.log(checked)
   return checked
 }
